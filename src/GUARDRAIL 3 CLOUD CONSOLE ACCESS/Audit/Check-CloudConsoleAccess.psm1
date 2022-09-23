176f9e458b1d20732cf04dda15628f0ae2e27628
Jose Fehse <jose@fehse.ca>
2022-09-23 10:05:00 -0400

    Module3 - Incorporating try/catch and formatting (#155)
    * Fixed issue #32
    
    * Mitigation for modules 8 and 9 - initial draft
    
    * Added Mititgation and Report Time to modules 8,9 and 11
    
    * Added Report Time to all modules.
    
    * updated main with ReportTIme
    
    * missed a file
    
    * Updated create-manifest script.
    
    * fixed az.accounts module version
    
    * fixed automatic Subnet exclusion issue and added CBS subscription exclusion to module 8
    
    * Fixed PBMM Module, added Report Time and CBS subscriptione exclusion to module 5-9 and 11.
    
    * New zips
    
    * Fixed minor parameter issues with main.ps1
    
    * Fixed issues
    
    * Fixed issues
    
    * Added proper permissions to MI for tenant diagnostics
    
    * merging from main
    
    * Adding Mititgation Commands/Instructions
    
    * Adding Mitigation commands
    
    * Fixed module eleven
    
    * Fixing zip issue
    
    * Fixed documentationa and added check on parameter format
    
    * Set up CI with Azure Pipelines
    
    [skip ci]
    
    * Added comment regarding permissions to documention.
    
    * Completed Module approach changes
    
    * minor fixes.
    
    * added module parameters and updated doc.
    
    * update
    
    * setup update
    
    * added tags.json
    
    * Documentation updates
    
    * Added tags documentation
    
    * Fixed module 10
    
    * Added zip for fixed module 10
    
    * Complete re-writing of modules 5,6 and 7
    
    * Removed unnecessary read me files for modules 6 and 7
    
    * minor fixes in main and update.ps1
    
    * minor fixes in 5,6,7
    
    * Change datetime format.
    
    * fixed typo in bicep file
    
    * Added ItemName to Module 10
    
    * Updated workbook
    
    * Added new CodeOwner
    
    * Updated query time to 24 hours.
    
    * minor fix to module 10
    
    * trying to merge...
    
    * Fixing AzAccounts issue
    
    * Fixed query for workbook
    
    * fixing workbook
    
    * Updated PBMMM and DiTransit zips
    
    * new zips for test
    
    * Updated localization zip
    
    * Added module 3
    
    * zipped language pack
    
    * modules.json fix
    
    * Update Check-CloudConsoleAccess.psm1
    
    * Added module 3
    
    * fixed conflicts with main
    
    Co-authored-by: Islam Gomaa <igomaa@users.noreply.github.com>

--- src/GUARDRAIL 3 CLOUD CONSOLE ACCESS/Audit/Check-CloudConsoleAccess.psm1
+++ src/GUARDRAIL 3 CLOUD CONSOLE ACCESS/Audit/Check-CloudConsoleAccess.psm1
@@ -2,79 +2,88 @@
 function Get-CloudConsoleAccess {
     param (      
         #[Parameter(Mandatory=$true)]
-[string] $ControlName,
+        [string] $ControlName,
         #[Parameter(Mandatory=$true)]
-[string] $ItemName,
+        [string] $ItemName,
         #[Parameter(Mandatory=$true)]
-[string] $WorkSpaceID,
+        [string] $WorkSpaceID,
         #[Parameter(Mandatory=$true)]
-[string] $workspaceKey,
+        [string] $workspaceKey,
         #[Parameter(Mandatory=$true)]
-[string] $LogType,
+        [string] $LogType,
         #[Parameter(Mandatory=$true)]
-[string] $itsgcode,
+        [string] $itsgcode,
         #[Parameter(Mandatory=$true)]
-[hashtable] $msgTable,
+        [hashtable] $msgTable,
         #[Parameter(Mandatory=$true)]
-[string] $ReportTime
+        [string] $ReportTime
     )
     #[PSCustomObject] $FinalObjectList = New-Object System.Collections.ArrayList
     [String] $GraphAccessToken = (Get-AzAccessToken -ResourceTypeName MSGraph).Token
-    $IsCompliant=$false
+    $IsCompliant = $false
     Write-Debug "Token: $GraphAccessToken"
     $locationsBaseAPIUrl = "https://graph.microsoft.com/v1.0/identity/conditionalAccess/namedLocations"
-    $CABaseAPIUrl="https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies"
-
-    $locations=(Invoke-RestMethod -Headers @{Authorization = "Bearer $($GraphAccessToken)" } -Uri $locationsBaseAPIUrl -Method Get -ErrorAction Stop).value
-    #$locations
-    $validLocations=@()
-    foreach ($location in $locations)
-    {
+    $CABaseAPIUrl = "https://graph.microsoft.com/v1.0/identity/conditionalAccess/policies"
+    try {
+        $locations = (Invoke-RestMethod -Headers @{Authorization = "Bearer $($GraphAccessToken)" } -Uri $locationsBaseAPIUrl -Method Get -ErrorAction Stop).value
+    }
+    catch {
+        Add-LogEntry 'Error' "Failed to call Microsoft Graph REST API at URL '$locationsBaseAPIUrl'; returned error message: $_" -workspaceGuid $WorkSpaceID -workspaceKey $WorkSpaceKey
+        Write-Error "Error: Failed to call Microsoft Graph REST API at URL '$locationsBaseAPIUrl'; returned error message: $_"
+    }
+    # $locations
+    $validLocations = @()
+    foreach ($location in $locations) {
         #Determine location conditions
         #get all valid locations: needs to have Canada Only
-        if ($location.countriesAndRegions.Count -eq 1 -and $location.countriesAndRegions -eq "CA")
-        {
-            $validLocations+=$location
+        if ($location.countriesAndRegions.Count -eq 1 -and $location.countriesAndRegions -eq "CA") {
+            $validLocations += $location
         }
     }
-    if ($validLocations.count -ne 0)
-    {
+    if ($validLocations.count -ne 0) {
         #if there is at least one location with Canada only, we are good. If no Canada Only policy, not compliant.
         # Conditional access Policies
         # Need a location based policy, for admins (owners, contributors) that uses one of the valid locations above.
         # If there is no policy or the policy doesn't use one of the locations above, not compliant.
-        $caps=(Invoke-RestMethod -Headers @{Authorization = "Bearer $($GraphAccessToken)" } -Uri $CABaseAPIUrl -Method Get -ErrorAction Stop).value
-        $validPolicies = $caps | Where-Object {$_.conditions.locations.includeLocations -in $validLocations.ID -and $cap.state -eq 'enabled'}
-        if (!$validPolicies)
-        {
+
+        try {
+            $caps = (Invoke-RestMethod -Headers @{Authorization = "Bearer $($GraphAccessToken)" } -Uri $CABaseAPIUrl -Method Get -ErrorAction Stop).value
+
+            $validPolicies = $caps | Where-Object { $_.conditions.locations.includeLocations -in $validLocations.ID -and $cap.state -eq 'enabled' }
+        }
+        catch {
+            Add-LogEntry 'Error' "Failed to call Microsoft Graph REST API at URL '$CABaseAPIUrl'; returned error message: $_" -workspaceGuid $WorkSpaceID -workspaceKey $WorkSpaceKey
+            Write-Error "Error: Failed to call Microsoft Graph REST API at URL '$CABaseAPIUrl'; returned error message: $_"
+        }
+        if (!$validPolicies) {
             #failed. No policies have valid locations.
-            $Comments=$msgTable.noCompliantPoliciesfound
-            $IsCompliant=$false
+            $Comments = $msgTable.noCompliantPoliciesfound
+            $IsCompliant = $false
         }
         else {
             "Compliant Policies."
             $IsCompliant = $true
-            $Comments=$msgTable.allPoliciesAreCompliant
+            $Comments = $msgTable.allPoliciesAreCompliant
         }      
     }
     else {
         # Failed. Reason: No locations have only Canada.
-        $Comments=$msgTable.noLocationsCompliant
-        $IsCompliant=$false
+        $Comments = $msgTable.noLocationsCompliant
+        $IsCompliant = $false
     }
     $PsObject = [PSCustomObject]@{
-        ComplianceStatus= $IsCompliant
-        ControlName = $ControlName
-        Comments= $Comments
-        ItemName= $ItemName
-        ReportTime = $ReportTime
-        itsgcode = $itsgcode
-     }
-     $JsonObject = convertTo-Json -inputObject $PsObject 
+        ComplianceStatus = $IsCompliant
+        ControlName      = $ControlName
+        Comments         = $Comments
+        ItemName         = $ItemName
+        ReportTime       = $ReportTime
+        itsgcode         = $itsgcode
+    }
+    $JsonObject = convertTo-Json -inputObject $PsObject 
 
-     Send-OMSAPIIngestionFile  -customerId $WorkSpaceID `
-                               -sharedkey $workspaceKey `
-                               -body $JsonObject `
-                               -logType $LogType `
-                               -TimeStampField Get-Date 
+    Send-OMSAPIIngestionFile  -customerId $WorkSpaceID `
+        -sharedkey $workspaceKey `
+        -body $JsonObject `
+        -logType $LogType `
+        -TimeStampField Get-Date 
 }
