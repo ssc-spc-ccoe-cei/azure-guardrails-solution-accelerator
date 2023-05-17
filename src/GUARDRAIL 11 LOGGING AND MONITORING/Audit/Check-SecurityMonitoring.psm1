e885b4056224e997b46298bb964146eaa60e6dec
Matthew Bratschun <25390936+mbrat2005@users.noreply.github.com>
2023-05-17 08:53:24 -0600

    Fix Signature Cleanup Bogle on GR11 an GR4 (#379)
    * added gr 11 files from v1.1.3
    
    * added gr 4 files from v1.1.3

--- src/GUARDRAIL 11 LOGGING AND MONITORING/Audit/Check-SecurityMonitoring.psm1
+++ src/GUARDRAIL 11 LOGGING AND MONITORING/Audit/Check-SecurityMonitoring.psm1
@@ -1,3 +1,228 @@
 #Future Params:
+#Security
+function get-apiLinkedServicesData {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $subscriptionId,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $resourceGroup,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $LAWName
+    )
+    $apiUrl="https://management.azure.com/subscriptions/$subscriptionId/resourcegroups/$resourceGroup/providers/Microsoft.OperationalInsights/workspaces/$LAWName/linkedServices?api-version=2020-08-01"
+    try {
+        $response = Invoke-AzRestMethod -Uri $apiUrl -Method Get
+    }
+    catch {
+        Write-Error "Error: Failed to call Azure Resource Manager REST API at URL '$apiURL'; returned error message: $_"
+    }
+
+    $data = $response.Content | ConvertFrom-Json
+    return $data
 }
+
+function get-tenantDiagnosticsSettings {
+
+    $apiUrl = "https://management.azure.com/providers/microsoft.aadiam/diagnosticSettings?api-version=2017-04-01-preview"
+    try {
+        $response = Invoke-AzRestMethod -Uri $apiUrl -Method Get -ErrorAction Stop
+    }
+    catch {
+        Write-Error "Error: Failed to call Azure Resource Manager REST API at URL '$apiURL'; returned error message: $_"
+    }
+
+    $data = $response.Content | ConvertFrom-Json
+    return $data.value.properties
+}
+function get-activitylogstatus {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $LAWResourceId
+    )
+    
+    $subs=Get-AzSubscription -ErrorAction SilentlyContinue| Where-Object {$_.State -eq "Enabled"}
+    $totalsubs=$subs.Count
+
+    $pcount=0
+    foreach ($sub in $subs) {
+        $URL="https://management.azure.com/subscriptions/$($sub.Id)/providers/Microsoft.Insights/diagnosticSettings?api-version=2021-05-01-preview"
+        
+        $response = Invoke-AzRestMethod -Uri $URL -Method Get 
+        
+        $data = $response.Content | ConvertFrom-Json
+        $configuredWSs = $data.value.Properties.workspaceId
+        if ($LAWResourceId -in $configuredWSs) {
+            $pcount++
+        }
+    }
+    if ($pcount -ne $totalsubs) {
+        Write-Warning "Not all subscriptions are configured to send logs to the Log Analytics Workspace"
+        return $false
+    }
+    else {
+        Write-Host "All subscriptions are configured to send logs to the Log Analytics Workspace"
+        return $true
+    }
+}
+function get-SecurityMonitoringStatus {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $SecurityLAWResourceId,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ControlName,
+        [string] $itsginfosecmon,
+        [hashtable]
+        $msgTable,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $ReportTime,
+        [Parameter(Mandatory=$true)]
+        [string]
+        $CBSSubscriptionName,
+        [Parameter(Mandatory=$false)]
+        [int]
+        $LAWRetention=730
+    )
+    [PSCustomObject] $FinalObjectList = New-Object System.Collections.ArrayList
+    [PSCustomObject] $ErrorList = New-Object System.Collections.ArrayList
+    #$LogType="GuardrailsCompliance"
+    #Code
+
+    #Add test for proper right format of the LAW parameters
+    $Subscription=$SecurityLAWResourceId.Split("/")[2]
+    $LAWRG=$SecurityLAWResourceId.Split("/")[4]
+    $LAWName=$SecurityLAWResourceId.Split("/")[8]
+    
+    $IsCompliant=$true
+    
+    try{
+        Select-AzSubscription -Subscription $Subscription -ErrorAction Stop | Out-Null
+    }
+    catch {
+        $ErrorList.Add("Failed to execute the 'Select-AzSubscription' command with subscription ID '$($subscription)'--`
+            ensure you have permissions to the subscription, the ID is correct, and that it exists in this tenant; returned `
+            error message: $_")
+        #    ensure you have permissions to the subscription, the ID is correct, and that it exists in this tenant; returned `
+        #    error message: $_"
+        throw "Error: Failed to execute the 'Select-AzSubscription' command with subscription ID '$($subscription)'--ensure `
+            you have permissions to the subscription, the ID is correct, and that it exists in this tenant; returned error message: $_"
+    }
+
+    try {
+        $LAW=Get-AzOperationalInsightsWorkspace -Name $LAWName -ResourceGroupName $LAWRG -ErrorAction Stop
+    }
+    catch {
+        $ErrorList.Add("Failed to retrieve Log Analytics workspace '$LAWName' from resource group '$LAWRG'--verify that the `
+        workspace exists and that permissions are sufficient; returned error message: $_")
+        #    workspace exists and that permissions are sufficient; returned error message: $_"
+    }
+    if ($null -eq $LAW)
+    {
+        $IsCompliant=$false
+        $Comments=$msgTable.securityLAWNotFound
+        # $MitigationCommands = $msgTable.createLAW
+    }
+    else {
+        # Test linked automation account
+        $LinkedServices=get-apiLinkedServicesData -subscriptionId $Subscription `
+            -resourceGroup $LAWRG `
+            -LAWName $LAWName
+        if (($LinkedServices.value.properties.resourceId | Where-Object {$_ -match "automationAccounts"}).count -lt 1)
+        {
+            $IsCompliant=$false
+            $Comments+=$msgTable.lawNoAutoAcct #"No linked automation account has been found."
+            # $MitigationCommands+=@"
+# $($msgTable.connectAutoAcct) ($LAWName).
+# https://docs.microsoft.com/en-us/azure/automation/quickstarts/create-account-portal
+# https://docs.microsoft.com/en-us/azure/automation/how-to/region-mappings
+# `n
+# "@
+        }
+        #Test Retention Days
+        $Retention=$LAW.retentionInDays
+        if ($Retention -ne $LAWRetention)
+        {
+            $IsCompliant=$false
+            $Comments+=$msgTable.lawRetention730Days
+            # $MitigationCommands += "$($msgTable.setRetention730Days) ($LAWName) -https://docs.microsoft.com/en-us/azure/azure-monitor/logs/data-retention-archive?tabs=api-1%2Capi-2 `n"
+        }
+        #Verify presense of the Activity Logs as a source
+        #Verify presense of the Activity Logs as a source
+        #old way:
+        #$ActivityLogDS=Get-AzOperationalInsightsDataSource -Workspace $LAW -Kind AzureActivityLog
+        #If ($ActivityLogDS -eq $null)
+        #{
+        #    $IsCompliant=$false
+        #    $Comments+=$msgTable.lawNoActivityLogs
+        #    $MitigationCommands+="$($msgTable.addActivityLogs) ($LAWName) - https://docs.microsoft.com/en-us/azure/active-directory/reports-monitoring/howto-analyze-activity-logs-log-analytics  `n"
+        #}
+        #New way:
+        if (!(get-activitylogstatus -LAWResourceId $LAW.ResourceId)) {
+            $IsCompliant=$false
+            $Comments+=$msgTable.lawNoActivityLogs
+            # $MitigationCommands+="$($msgTable.addActivityLogs) ($LAWName) - https://docs.microsoft.com/en-us/azure/active-directory/reports-monitoring/howto-analyze-activity-logs-log-analytics  `n"
+        }
+        # Tests for required Solutions
+        $enabledSolutions=(Get-AzOperationalInsightsIntelligencePack -ResourceGroupName $LAW.ResourceGroupName -WorkspaceName $LAW.Name| Where-Object {$_.Enabled -eq "True"}).Name
+        if ($enabledSolutions -notcontains "Updates" -or $enabledSolutions -notcontains "AntiMalware")
+        {
+            $IsCompliant=$false
+            $Comments+=$msgTable.lawSolutionNotFound # "Required solutions not present in the Log Analytics Workspace."
+<#            $MitigationCommands+=@"
+$($msgTable.addUpdatesAndAntiMalware) ($LAWName)"
+https://docs.microsoft.com/en-us/azure/automation/update-management/overview
+https://azuremarketplace.microsoft.com/en-us/marketplace/apps/Microsoft.AntiMalwareOMS?tab=Overview
+`n
+"@#>
+        }
+        # Tenant Diagnostics configuration. Needs Graph API...
+        $tenantWS=get-tenantDiagnosticsSettings
+        if ($SecurityLAWResourceId -notin $tenantWS.workspaceId)
+        {
+            $IsCompliant=$false
+            $Comments+=$msgTable.lawNoTenantDiag # "Tenant Diagnostics settings are not pointing to the provided log analysitcs workspace."
+            # $MitigationCommands+="$($msgTable.configTenantDiag) ($LAWName) https://docs.microsoft.com/en-us/azure/active-directory/reports-monitoring/howto-integrate-activity-logs-with-log-analytics#send-logs-to-azure-monitor  `n"
+        }
+        else {
+            #Workspace is there but need to check if logs are enabled.
+            $enabledLogs=(($tenantWS| Where-Object {$_.workspaceId -eq $SecurityLAWResourceId}).logs | Where-Object {$_.enabled -eq $true}).category
+            if ("AuditLogs" -notin $enabledLogs -or "SignInLogs" -notin $enabledLogs)
+            {
+                $IsCompliant=$false
+                $Comments+=$msgTable.lawMissingLogTypes # "Workspace set in tenant config but not all required log types are enabled (Audit and signin)."
+                # $MitigationCommands+="$($msgTable.addAuditAndSignInsLogs) ($LAWName) - https://docs.microsoft.com/en-us/azure/active-directory/reports-monitoring/howto-integrate-activity-logs-with-log-analytics#send-logs-to-azure-monitor `n"
+            }
+        }
+        #Blueprint redirection
+        # Sentinel, not sure how to detect this.
+        if ($IsCompliant)
+        {
+            $Comments= $msgTable.logsAndMonitoringCompliantForSecurity
+            # $MitigationCommands+="N/A"
+        }
+        $object = [PSCustomObject]@{ 
+            ComplianceStatus = $IsCompliant
+            Comments = $Comments
+            ItemName = $msgTable.securityMonitoring
+            itsgcode = $itsginfosecmon
+            ControlName = $ControlName
+            ReportTime = $ReportTime
+            # MitigationCommands=$MitigationCommands
+        }
+        $FinalObjectList+=$object
+        $IsCompliant=$true
+    }
+
+    $moduleOutput= [PSCustomObject]@{ 
+        ComplianceResults = $FinalObjectList 
+        Errors=$ErrorList
+        AdditionalResults = $AdditionalResults
+    }
     return $moduleOutput
+}
