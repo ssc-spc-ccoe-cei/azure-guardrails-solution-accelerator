c914c0e46a624fe25722beb712c863d11fbb26e8
singhgss <146129702+singhgss@users.noreply.github.com>
2023-10-24 15:54:16 -0400

    Fetch runtime config from keyvault to set AZ context to correct subscription (#453)

--- setup/main.ps1
+++ setup/main.ps1
@@ -92,6 +92,13 @@ If ($Locale -eq $null) {
     $Locale = "en-CA"
 }
 
+try {
+    $RuntimeConfig = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name 'gsaConfigExportLatest' -AsPlainText -ErrorAction Stop | ConvertFrom-Json | Select-Object -Expand runtime
+    Set-AzContext -SubscriptionId $RuntimeConfig.subscriptionId
+}
+catch {
+    throw "Failed to retrieve config json with secret name gsaConfigExportLatest from KeyVault '$KeyVaultName'. Error message: $_"
+}
 
 $SubID = (Get-AzContext).Subscription.Id
 $tenantID = (Get-AzContext).Tenant.Id
