fa5cc8c2e7ad26c61683588d21c20810a3e5f292
alalvi00 <ali.alvi@ssc-spc.gc.ca>
2024-02-12 13:12:20 -0500

    Created first unit test case

---
+++ tests/GUARDRAIL 1 PROTECT ROOT  GLOBAL ADMINS ACCOUNT/Audit/Check-BreakGlassAccountIdentityProtectionLicense.Tests.ps1
@@ -0,0 +1,31 @@
+Import-Module ".\src\GUARDRAIL 1 PROTECT ROOT  GLOBAL ADMINS ACCOUNT\Audit\Check-BreakGlassAccountIdentityProtectionLicense.psm1"
+
+Describe "Get-BreakGlassAccountLicense Function" {
+    $FirstBreakGlassUPN = "test123@hotmail.com"
+    $SecondBreakGlassUPN = "test321@hotmail.com"
+    $ControlName = "Guardrails1"
+    $ItemName = "Break Glass Microsoft Entra ID P2"
+    $itsgcode = "AC2(7)"
+
+    $msgTable = @{
+        firstBgAccount          = "First BG Account"
+        secondBgAccount         = "Second BG Account"
+        bgValidLicenseAssigned  = "has a valid license assigned"
+        bgNoValidLicenseAssigned = "does not have a valid license assigned"
+    }
+
+    It "Should return compliant results when both accounts have a valid license" {
+        $result = Get-BreakGlassAccountLicense -FirstBreakGlassUPN $FirstBreakGlassUPN -SecondBreakGlassUPN $SecondBreakGlassUPN -ControlName $ControlName -ItemName $ItemName -itsgcode $itsgcode -msgTable $msgTable -ReportTime "2024-01-01"
+        $result.ComplianceResults.ComplianceStatus | Should -Be $true
+    }
+
+    It "Should return non-compliant results when the first account does not have a valid license" {
+        $result = Get-BreakGlassAccountLicense -FirstBreakGlassUPN $FirstBreakGlassUPN -SecondBreakGlassUPN $SecondBreakGlassUPN -ControlName $ControlName -ItemName $ItemName -itsgcode $itsgcode -msgTable $msgTable -ReportTime "2024-01-01"
+        $result.ComplianceResults.ComplianceStatus | Should -Be $false
+    }
+
+    It "Should return non-compliant results when the second account does not have a valid license" {
+        $result = Get-BreakGlassAccountLicense -FirstBreakGlassUPN $FirstBreakGlassUPN -SecondBreakGlassUPN $SecondBreakGlassUPN -ControlName $ControlName -ItemName $ItemName -itsgcode $itsgcode -msgTable $msgTable -ReportTime "2024-01-01"
+        $result.ComplianceResults.ComplianceStatus | Should -Be $false
+    }
+}
