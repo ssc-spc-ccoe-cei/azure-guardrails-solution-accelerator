b1078e218a74b5abf7ea4651718b742fd24e09de
Gurpreet Singh <gurpreet.singh@ssc-spc.gc.ca>
2024-08-13 10:54:05 -0400

    update

--- src/GUARDRAIL 10 CYBER DEFENSE SERVICES/Audit/Check-CyberSecurityServices.psm1
+++ src/GUARDRAIL 10 CYBER DEFENSE SERVICES/Audit/Check-CyberSecurityServices.psm1
@@ -13,9 +13,9 @@ function Check-CBSSensors {
         $ModuleProfiles,  # Passed as a string
         [string] 
         $CloudUsageProfiles = "3",  # Passed as a string
-        [bool] 
-        $EnableMultiCloudProfiles = $false  # New feature flag, default to false    
-    ) 
+        [string] 
+        $EnableMultiCloudProfiles = "false"  # New feature flag, default to false
+        ) 
 
     $IsCompliant = $true 
     $Object = New-Object PSObject
