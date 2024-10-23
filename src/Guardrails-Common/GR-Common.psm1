05ed4b2b1bc33e1ac6d2c2b43fd57eed5a3f2a2d
dutt0 <147828743+dutt0@users.noreply.github.com>
2024-10-23 15:00:54 -0400

    [New control] GR2 | Validation 7 | Authentication Mechanisms: Risk Based Conditional Access Policies (#245)
    * initial new module
    
    * adding module
    
    * module
    
    * update
    
    * fix typo
    
    * bugfix
    
    * added as function
    
    * update
    
    * update
    
    * complete module
    
    * fix GR number
    
    * update
    
    * msg update
    
    * reuse as function
    
    * possible updates
    
    * update GR3
    
    * update with exclude
    
    * exclude groups logic update
    
    * update-reuse
    
    * update
    
    * update
    
    * added BG validation
    
    * update
    
    * update
    
    * update bicep

--- src/Guardrails-Common/GR-Common.psm1
+++ src/Guardrails-Common/GR-Common.psm1
@@ -877,6 +877,87 @@ function Get-AllUserAuthInformation{
 }
 
 
+
+# Function used for V2.0 GR2V7(M) andV1.0  GR3(R) cloud console access
+function Get-allowedLocationCAPCompliance {
+    param (
+        [array]$ErrorList,
+        [string] $IsCompliant
+    )
+
+    # get named locations
+    $locationsBaseAPIUrl = '/identity/conditionalAccess/namedLocations'
+    try {
+        $response = Invoke-GraphQuery -urlPath $locationsBaseAPIUrl -ErrorAction Stop
+        $data = $response.Content
+        $locations = $data.value
+    }
+    catch {
+        $Errorlist.Add("Failed to call Microsoft Graph REST API at URL '$locationsBaseAPIUrl'; returned error message: $_") 
+        Write-Warning "Error: Failed to call Microsoft Graph REST API at URL '$locationsBaseAPIUrl'; returned error message: $_"
+    }
+
+    # get conditional access policies
+    $CABaseAPIUrl = '/identity/conditionalAccess/policies'
+    try {
+        $response = Invoke-GraphQuery -urlPath $CABaseAPIUrl -ErrorAction Stop
+
+        $caps = $response.Content.value
+    }
+    catch {
+        $Errorlist.Add("Failed to call Microsoft Graph REST API at URL '$CABaseAPIUrl'; returned error message: $_")
+        Write-Warning "Error: Failed to call Microsoft Graph REST API at URL '$CABaseAPIUrl'; returned error message: $_"
+    }
+    
+    # check that a named location for Canada exists and that a policy exists that uses it
+    $validLocations = @()
+
+    foreach ($location in $locations) {
+        #Determine location conditions
+        #get all valid locations: needs to have Canada Only
+        if ($location.countriesAndRegions.Count -eq 1 -and $location.countriesAndRegions[0] -eq "CA") {
+            $validLocations += $location
+        }
+    }
+
+    $locationBasedPolicies = $caps | Where-Object { $_.conditions.locations.includeLocations -in $validLocations.ID -and $_.state -eq 'enabled' }
+
+    if ($validLocations.count -ne 0) {
+        #if there is at least one location with Canada only, we are good. If no Canada Only policy, not compliant.
+        # Conditional access Policies
+        # Need a location based policy, for admins (owners, contributors) that uses one of the valid locations above.
+        # If there is no policy or the policy doesn't use one of the locations above, not compliant.
+
+        if (!$locationBasedPolicies) {
+            #failed. No policies have valid locations.
+            $Comments = $msgTable.noCompliantPoliciesfound
+            $IsCompliant = $false
+        }
+        else {
+            #"Compliant Policies."
+            $IsCompliant = $true
+            $Comments = $msgTable.allPoliciesAreCompliant
+        }      
+    }
+    else {
+        # Failed. Reason: No locations have only Canada.
+        $Comments = $msgTable.noLocationsCompliant
+        $IsCompliant = $false
+    }
+    
+    $PsObject = [PSCustomObject]@{
+        ComplianceStatus = $IsCompliant
+        ControlName      = $ControlName
+        Comments         = $Comments
+        ItemName         = $ItemName
+        ReportTime       = $ReportTime
+        itsgcode         = $itsgcode
+        Errors           = $ErrorList
+    }
+    return  $PsObject
+
+}
+
 # endregion
 
 
