c50a7d96af98cf0905c5e7de3efd5d09ac92390f
Dutta <ila.dutta@ssc-spc.gc.ca>
2024-11-22 10:35:34 -0500

    error handling

--- src/GUARDRAIL 7 PROTECTION OF DATA-IN-TRANSIT/Audit/Check-StorageAccountTLSversion.psm1
+++ src/GUARDRAIL 7 PROTECTION OF DATA-IN-TRANSIT/Audit/Check-StorageAccountTLSversion.psm1
@@ -75,40 +75,46 @@ function Verify-TLSForStorageAccount {
         throw "Error: Failed to execute the 'Get-AzSubscription' command--verify your permissions and the installion of the Az.Resources module; returned error message: $_"
     }
 
-    $PSObjectList = Check-TLSversion -objList $objs -ControlName $ControlName -ItemName $ItemName -LogType $LogType -itsgcode $itsgcode -msgTable $msgTable -ReportTime $ReportTime 
+    try{
+        $PSObjectList = Check-TLSversion -objList $objs -ControlName $ControlName -ItemName $ItemName -LogType $LogType -itsgcode $itsgcode -msgTable $msgTable -ReportTime $ReportTime 
 
-    # Filter to keep only objects that have the 'subscriptionName' property
-    $PSObjectListCleaned = $PSObjectList | Where-Object { $_.PSObject.Properties["MinimumTlsVersion"] }
+        # Filter to keep only objects that have the 'subscriptionName' property
+        $PSObjectListCleaned = $PSObjectList | Where-Object { $_.PSObject.Properties["MinimumTlsVersion"] }
 
-    # find TLS version not equal to TLS1.2
-    $filteredPSObjectList = $PSObjectListCleaned | Where-Object { $_.MinimumTlsVersion -ne "TLS1_2" }
+        # find TLS version not equal to TLS1.2
+        $filteredPSObjectList = $PSObjectListCleaned | Where-Object { $_.MinimumTlsVersion -ne "TLS1_2" }
 
-    # Condition: all storage accounts are using TLS1.2
-    if ($filteredPSObjectList.Count -eq 0){
-        $IsCompliant = $true
-        $commentsArray = $msgTable.isCompliant + " " + $msgTable.storageAccValidTLS
-    }
-    else{
-        # Condition: isTLSLessThan1_2 = true if the TLSversionNumeric < 1.2
-        $filteredPSObjectList | ForEach-Object {
-            $_ | Add-Member -MemberType NoteProperty -Name isTLSLessThan1_2 -Value ($_.TLSversionNumeric -lt 1.2)
-            $_  
-        }
-        $storageAccWithTLSLessThan1_2 = $filteredPSObjectList | Where-Object { $_.IsTLSLessThan1_2 -eq $true }
-
-        # condition: storage accounts are all using TLS version 1.2 or higher
-        if ($storageAccWithTLSLessThan1_2.Count -eq 0){
+        # Condition: all storage accounts are using TLS1.2
+        if ($filteredPSObjectList.Count -eq 0){
             $IsCompliant = $true
             $commentsArray = $msgTable.isCompliant + " " + $msgTable.storageAccValidTLS
         }
         else{
-            ## keep a record for non-compliant storage acc names for reference
-            $nonCompliantstorageAccountNames = ($storageAccWithTLSLessThan1_2 | Select-Object -ExpandProperty StorageAccountName | ForEach-Object { $_ } ) -join ', '
-            Write-Host "Storage accounts which are using TLS1.1 or less: $nonCompliantstorageAccountNames"
-            $IsCompliant = $false
-            $commentsArray = $msgTable.isNotCompliant + " " + $msgTable.storageAccNotValidTLS
+            # Condition: isTLSLessThan1_2 = true if the TLSversionNumeric < 1.2
+            $filteredPSObjectList | ForEach-Object {
+                $_ | Add-Member -MemberType NoteProperty -Name isTLSLessThan1_2 -Value ($_.TLSversionNumeric -lt 1.2)
+                $_  
+            }
+            $storageAccWithTLSLessThan1_2 = $filteredPSObjectList | Where-Object { $_.IsTLSLessThan1_2 -eq $true }
+
+            # condition: storage accounts are all using TLS version 1.2 or higher
+            if ($storageAccWithTLSLessThan1_2.Count -eq 0){
+                $IsCompliant = $true
+                $commentsArray = $msgTable.isCompliant + " " + $msgTable.storageAccValidTLS
+            }
+            else{
+                ## keep a record for non-compliant storage acc names for reference
+                $nonCompliantstorageAccountNames = ($storageAccWithTLSLessThan1_2 | Select-Object -ExpandProperty StorageAccountName | ForEach-Object { $_ } ) -join ', '
+                Write-Host "Storage accounts which are using TLS1.1 or less: $nonCompliantstorageAccountNames"
+                $IsCompliant = $false
+                $commentsArray = $msgTable.isNotCompliant + " " + $msgTable.storageAccNotValidTLS
+            }
         }
     }
+    catch{
+        $Errorlist.Add("exception: $_")
+    }
+    
 
     $Comments = $commentsArray -join ";"
     
