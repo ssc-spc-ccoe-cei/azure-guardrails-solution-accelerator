50fc5df4fb415b326520d190f648aaeaa1d765a1
Dutta <ila.dutta@ssc-spc.gc.ca>
2025-05-27 11:48:36 -0400

    add verbose

--- src/GuardrailsSolutionAcceleratorSetup/modules/Deploy-GuardrailsSolutionAccelerator/Deploy-GuardrailsSolutionAccelerator.psm1
+++ src/GuardrailsSolutionAcceleratorSetup/modules/Deploy-GuardrailsSolutionAccelerator/Deploy-GuardrailsSolutionAccelerator.psm1
@@ -19,13 +19,13 @@ Function Invoke-GSARunbooks {
     )
 
     try {
-        Start-AzAutomationRunbook -Name "main" -AutomationAccountName $config['runtime']['autoMationAccountName'] -ResourceGroupName $config['runtime']['resourceGroup'] -ErrorAction Stop | Out-Null
+        Start-AzAutomationRunbook -Name "main" -AutomationAccountName $config['runtime']['autoMationAccountName'] -ResourceGroupName $config['runtime']['resourceGroup'] -Verbose -LogVerbose $true -LogProgress $true -ErrorAction Stop | Out-Null
     }
     catch { 
         Write-Error "Error starting 'main' runbook. $_"
     }
     try {
-        Start-AzAutomationRunbook -Name "backend" -AutomationAccountName $config['runtime']['autoMationAccountName'] -ResourceGroupName $config['runtime']['resourceGroup'] -ErrorAction Stop | Out-Null
+        Start-AzAutomationRunbook -Name "backend" -AutomationAccountName $config['runtime']['autoMationAccountName'] -ResourceGroupName $config['runtime']['resourceGroup'] -Verbose -LogVerbose $true -LogProgress $true -ErrorAction Stop | Out-Null
     }
     catch { 
         Write-Error "Error starting 'backend' runbook. $_"
