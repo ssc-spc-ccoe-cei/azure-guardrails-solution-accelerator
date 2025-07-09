8ddbc7f88f884002754f6cf73a264e036faa6909
Dutta <ila.dutta@ssc-spc.gc.ca>
2025-07-09 08:09:20 -0400

    update GR10

--- src/GUARDRAIL 10 CYBER DEFENSE SERVICES/Audit/Check-CyberSecurityServices.psm1
+++ src/GUARDRAIL 10 CYBER DEFENSE SERVICES/Audit/Check-CyberSecurityServices.psm1
@@ -17,14 +17,7 @@ function Check-CBSSensors {
 
     $IsCompliant = $true
     $Comments = ""
-    $Object = [PSCustomObject]@{
-        ComplianceStatus = $IsCompliant
-        ControlName = $ControlName
-        ItemName = $ItemName
-        Comments = $Comments
-        ReportTime = $ReportTime
-        itsgcode = $itsgcode
-    }
+
 
     $FirstTokenInTenantID = $TenantID.Split("-")[0]
     $CBSResourceNames = @(
@@ -39,61 +32,13 @@ function Check-CBSSensors {
     $sub = Get-AzSubscription -ErrorAction SilentlyContinue | Where-Object { $_.State -eq 'Enabled' -and $_.Name -eq $SubscriptionName }
     if ($null -eq $sub) {
         $IsCompliant = $false
-        $Object | Add-Member -MemberType NoteProperty -Name Comments -Value $msgTable.cbsSubDoesntExist
+        # $Object | Add-Member -MemberType NoteProperty -Name Comments -Value $msgTable.cbsSubDoesntExist
+        $Comments = $msgTable.cbsSubDoesntExist
         $MitigationCommands = $msgTable.cbssMitigation -f $SubscriptionName
-        if ($EnableMultiCloudProfiles) {
-            $evalResult = Get-EvaluationProfile -CloudUsageProfiles $CloudUsageProfiles -ModuleProfiles $ModuleProfiles -SubscriptionId $sub.Id
-            if (!$evalResult.ShouldEvaluate) {
-                if(!$evalResult.ShouldAvailable ){
-                    if ($evalResult.Profile -gt 0) {
-                        $Object.ComplianceStatus = "Not Available"
-                        $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-                        $Object.Comments = "Not available - Profile $($evalResult.Profile) not applicable for this guardrail"
-                    } else {
-                        $ErrorList.Add("Error occurred while evaluating profile configuration availability")
-                    }
-                } else {
-                    if ($evalResult.Profile -gt 0) {
-                        $Object.ComplianceStatus = "Not Applicable"
-                        $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-                        $Object.Comments = "Not evaluated - Profile $($evalResult.Profile) not present in CloudUsageProfiles"
-                    } else {
-                        $ErrorList.Add("Error occurred while evaluating profile configuration")
-                    }
-                }
-            } else {
-                
-                $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-            }
-        }
+        
     } else {
         Set-AzContext -Subscription $sub
 
-        if ($EnableMultiCloudProfiles) {        
-            $evalResult = Get-EvaluationProfile -CloudUsageProfiles $CloudUsageProfiles -ModuleProfiles $ModuleProfiles -SubscriptionId $sub.Id
-            if (!$evalResult.ShouldEvaluate) {
-                if(!$evalResult.ShouldAvailable ){
-                    if ($evalResult.Profile -gt 0) {
-                        $Object.ComplianceStatus = "Not Available"
-                        $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-                        $Object.Comments = "Not available - Profile $($evalResult.Profile) not applicable for this guardrail"
-                    } else {
-                        $ErrorList.Add("Error occurred while evaluating profile configuration availability")
-                    }
-                } else {
-                    if ($evalResult.Profile -gt 0) {
-                        $Object.ComplianceStatus = "Not Applicable"
-                        $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-                        $Object.Comments = "Not evaluated - Profile $($evalResult.Profile) not present in CloudUsageProfiles"
-                    } else {
-                        $ErrorList.Add("Error occurred while evaluating profile configuration")
-                    }
-                }
-            } else {
-                
-                $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-            }
-        }
         foreach ($CBSResourceName in $CBSResourceNames) {
             if ($debug) { Write-Output "Searching for CBS Sensor: $CBSResourceName" }
             if ([string]::IsNullOrEmpty((Get-AzResource -Name $CBSResourceName))) {
@@ -104,20 +49,56 @@ function Check-CBSSensors {
         }
 
         if ($IsCompliant) {
-            $Object | Add-Member -MemberType NoteProperty -Name Comments -Value "$($msgTable.cbssCompliant) $SubscriptionName)"| Out-Null
+            $Comments = "$($msgTable.cbssCompliant) $SubscriptionName"
             $MitigationCommands = "N/A."
-        } else {
-            $Object | Add-Member -MemberType NoteProperty -Name Comments -Value $Comment2 | Out-Null   
+        } else { 
             $MitigationCommands = "Contact CBS to deploy sensors."
         }
     }
 
-    $Object | Add-Member -MemberType NoteProperty -Name ComplianceStatus -Value $IsCompliant| Out-Null
+    $Object = [PSCustomObject]@{
+        ComplianceStatus = $IsCompliant
+        ControlName = $ControlName
+        Comments = $Comments
+        ItemName = $ItemName
+        ReportTime = $ReportTime
+        itsgcode = $itsgcode
+    }
     $Object | Add-Member -MemberType NoteProperty -Name MitigationCommands -Value $MitigationCommands| Out-Null
 
-    [PSCustomObject]@{ 
+    if ($EnableMultiCloudProfiles) {
+
+        $evalResult = Get-EvaluationProfile -CloudUsageProfiles $CloudUsageProfiles -ModuleProfiles $ModuleProfiles -SubscriptionId $sub.Id
+        if (!$evalResult.ShouldEvaluate) {
+            if(!$evalResult.ShouldAvailable ){
+                if ($evalResult.Profile -gt 0) {
+                    $Object.ComplianceStatus = "Not Available"
+                    $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
+                    $Object.Comments = "Not available - Profile $($evalResult.Profile) not applicable for this guardrail"
+                } else {
+                    $ErrorList.Add("Error occurred while evaluating profile configuration availability")
+                }
+            } else {
+                if ($evalResult.Profile -gt 0) {
+                    $Object.ComplianceStatus = "Not Applicable"
+                    $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
+                    $Object.Comments = "Not evaluated - Profile $($evalResult.Profile) not present in CloudUsageProfiles"
+                } else {
+                    $ErrorList.Add("Error occurred while evaluating profile configuration")
+                }
+            }
+        } else {
+            
+            $Object | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
+        }
+    }
+
+    $moduleOutput = [PSCustomObject]@{ 
         ComplianceResults = $Object 
         Errors = $ErrorList
         AdditionalResults = $AdditionalResults
     }
+    
+    return $moduleOutput
+
 }
