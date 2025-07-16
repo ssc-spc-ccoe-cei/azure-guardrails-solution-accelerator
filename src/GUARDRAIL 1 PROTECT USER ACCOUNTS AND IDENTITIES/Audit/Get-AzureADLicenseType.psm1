b73cf8011a6bc3f37b135126709d943710a1222d
Dutta <ila.dutta@ssc-spc.gc.ca>
2025-07-16 10:32:51 -0400

    refactor GR1

--- src/GUARDRAIL 1 PROTECT USER ACCOUNTS AND IDENTITIES/Audit/Get-AzureADLicenseType.psm1
+++ src/GUARDRAIL 1 PROTECT USER ACCOUNTS AND IDENTITIES/Audit/Get-AzureADLicenseType.psm1
@@ -68,33 +68,11 @@ function Get-ADLicenseType {
         Comments = $Comments
      }
 
-    # Conditionally add the Profile field based on the feature flag
+    # #Conditionally add the Profile field based on the feature flag
     if ($EnableMultiCloudProfiles) {
-        $evalResult = Get-EvaluationProfile -CloudUsageProfiles $CloudUsageProfiles -ModuleProfiles $ModuleProfiles
-        if (!$evalResult.ShouldEvaluate) {
-            if(!$evalResult.ShouldAvailable ){
-                if ($evalResult.Profile -gt 0) {
-                    $PsObject.ComplianceStatus = "Not Available"
-                    $PsObject | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-                    $PsObject.Comments = "Not available - Profile $($evalResult.Profile) not applicable for this guardrail"
-                } else {
-                    $ErrorList.Add("Error occurred while evaluating profile configuration availability")
-                }
-            } else {
-                if ($evalResult.Profile -gt 0) {
-                    $PsObject.ComplianceStatus = "Not Applicable"
-                    $PsObject | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-                    $PsObject.Comments = "Not evaluated - Profile $($evalResult.Profile) not present in CloudUsageProfiles"
-                } else {
-                    $ErrorList.Add("Error occurred while evaluating profile configuration")
-                }
-            }
-        } else {
-            
-            $PsObject | Add-Member -MemberType NoteProperty -Name "Profile" -Value $evalResult.Profile
-        }
+        $result = Add-ProfileInformation -Result $PsObject -CloudUsageProfiles $CloudUsageProfiles -ModuleProfiles $ModuleProfiles -SubscriptionId $subscriptionId
+        Write-Host "$result"
     }
-
 
     $moduleOutput= [PSCustomObject]@{ 
         ComplianceResults = $PsObject
