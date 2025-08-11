08c1869fa29ee86e12cf09c02f391e3814928642
amrinderssc <amrinder.singh@ssc-spc.gc.ca>
2025-08-11 08:48:30 -0600

    CentralViewQuery lookup time fix from 6h to 24h in the past

--- tools/CentralView/Modules/ingest-tenantsData/ingest-tenantsData.psm1
+++ tools/CentralView/Modules/ingest-tenantsData/ingest-tenantsData.psm1
@@ -61,7 +61,7 @@ function get-tenantdata {
     let ctrlprefix="GUARDRAIL 8";
     GuardrailsCompliance_CL
     | where ControlName_s has ctrlprefix and ReportTime_s == "{0}"
-    | where TimeGenerated > ago (6h)
+    | where TimeGenerated > ago (24h)
     |join kind=inner (itsgcodes) on itsgcode_s
     | project Mandatory=Required_s,ControlName_s, SubnetName=SubnetName_s, ItemName=ItemName_s, Profile=column_ifexists('Profile_d',''), Status=case(
         column_ifexists('ComplianceStatus_s', '') == "Not Applicable", "Not Applicable",
@@ -121,7 +121,7 @@ function get-tenantdata {
         if ($TenantDomain -ne "" -and $TenantDomain -ne "N/A")
         {
 
-            $ReportTimeQuery="GuardrailsCompliance_CL | where TimeGenerated > ago(6h)| summarize mrt=max(ReportTime_s)"
+            $ReportTimeQuery="GuardrailsCompliance_CL | where TimeGenerated > ago(24)| summarize mrt=max(ReportTime_s)"
             $resultsArray = [System.Linq.Enumerable]::ToArray((Invoke-AzOperationalInsightsQuery -WorkspaceId $ws -Query $ReportTimeQuery).Results)   
             $LatestRT=$resultsArray[0].mrt
             if ([string]::IsNullOrEmpty($LatestRT)) {
