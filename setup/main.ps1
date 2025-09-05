99dcaa97b99aab4c7a39dd0cd58f21df394fd071
Islam Gomaa <islam.gomaa@ssc-spc.gc.ca>
2025-09-05 15:45:56 -0400

    remove debuging from main

--- setup/main.ps1
+++ setup/main.ps1
@@ -258,7 +258,7 @@ foreach ($module in $modules) {
             #$results.ComplianceResults
             #$results.Add("Required", $module.Required)
             #Write-Output "required in module: $($module.Required)."
-            $results
+            #$results
             $results.ComplianceResults | Add-Member -MemberType NoteProperty -Name "Required" -Value $module.Required -PassThru
             
             #Write-Output "required in results: $($results.Required)."
